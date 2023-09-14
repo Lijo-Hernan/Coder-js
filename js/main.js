1cf5782c55828db7dfa697ac58f88cba5882c27b
Hernán Pablo Lijó <lijo.hernanpablo@gmail.com>
2023-09-14 02:17:22 -0300

    version sin frameworks

--- js/main.js
+++ js/main.js
@@ -21,23 +21,23 @@ const horarios = [
 ];
 const medicos = [
     {
-        dr: "Dr. Bruno H",
+        dr: " Dr. Bruno H",
         servicio: "Clinica Medica",
     },
     {
-        dr: "Dra. Prado S.",
+        dr: " Dra. Prado S.",
         servicio: "Ginecologia",
     },
     {
-        dr: "Dra. Rozas S.",
+        dr: " Dra. Rozas S.",
         servicio: "Traumatologia",
     },
     {
-        dr: "Dr. Gilardoni A.",
+        dr: " Dr. Gilardoni A.",
         servicio: "Cirugia General",
     },
     {
-        dr: "Dr. Sassano A.",
+        dr: " Dr. Sassano A.",
         servicio: "Urologia",
     },
 ];
@@ -64,44 +64,51 @@ function agregarPaciente(nombre, apellido) {
     arrayPaciente.push(paciente1);
     turnoOtorgado.push(paciente1);
     mostrarEspecialidades();
+
+    let displayLista = document.getElementById("lista");
+    displayLista.style.display = "flex";
 }
 let especialidadSeleccionada;
 
 function mostrarEspecialidades() {
-    if (arrayPaciente.length > 0) {
+    if (arrayPaciente.length > 0 && turnoOtorgado.length > 0) {
         let ulEspecialidad = document.getElementById("listaEspecialidad");
         let spanEspecialidad = document.getElementById("spanEspecialidad");
 
-        spanEspecialidad.innerHTML = `Bienvenido ${nombrePaciente.toUpperCase()} ${apellidoPaciente.toUpperCase()} , seleccione una especialidad para la que necesite gestionar un turno<br><br>`;
+        spanEspecialidad.innerHTML = `Bienvenido <strong>${nombrePaciente.toUpperCase()} ${apellidoPaciente.toUpperCase()}</strong>, seleccione una especialidad para la que necesite gestionar un turno<br><br>`;
 
         especialidades.forEach(function (especialidad, index) {
             let selectEspecialidad = document.createElement("input");
             selectEspecialidad.type = "radio";
-            selectEspecialidad.id = `especialidad` + (index);
+            selectEspecialidad.id = `especialidad` + index;
             selectEspecialidad.name = "especialidad";
             selectEspecialidad.value = `${especialidad}`;
 
             let label = document.createElement("label");
-            label.setAttribute("for", "especialidad" + (index));
+            label.setAttribute("for", "especialidad" + index);
             label.textContent = especialidad;
-
 
             ulEspecialidad.appendChild(selectEspecialidad);
             ulEspecialidad.appendChild(label);
             ulEspecialidad.appendChild(document.createElement("br"));
         });
 
-        let seleccion1 = document.querySelectorAll('input[type="radio"][name="especialidad"]');
+        let seleccion1 = document.querySelectorAll(
+            'input[type="radio"][name="especialidad"]'
+        );
 
         seleccion1.forEach(function (radio) {
             radio.addEventListener("change", function () {
-                if (radio.checked) { especialidadSeleccionada = radio.value; }
+                if (radio.checked) {
+                    especialidadSeleccionada = radio.value;
+                }
                 agregarEspecialidad(especialidadSeleccionada);
             });
         });
+        let displayForm = document.getElementById("datosPaciente");
+        displayForm.style.display = "none";
     }
-};
-
+}
 
 function agregarEspecialidad(especialidadSeleccionada) {
     especialidadSeleccionada = {
@@ -111,24 +118,24 @@ function agregarEspecialidad(especialidadSeleccionada) {
     seleccionDia();
 }
 
-let diaSeleccionado
+let diaSeleccionado;
 
 function seleccionDia() {
     if (especialidadSeleccionada != undefined) {
         let ulDia = document.getElementById("listaDias");
 
         let spanDia = document.getElementById("spanDia");
-        spanDia.innerHTML = `Por favor seleccione un Dia disponible para su un turno en <br><br>`;
+        spanDia.innerHTML = `Por favor seleccione un Dia disponible para su un turno en <strong>${especialidadSeleccionada}</strong> <br><br>`;
 
         dias.forEach(function (dia, index) {
             let selectDia = document.createElement("input");
             selectDia.type = "radio";
             selectDia.name = "dias";
-            selectDia.id = "dias" + (index);
+            selectDia.id = "dias" + index;
             selectDia.value = `${dia}`;
 
             let label = document.createElement("label");
-            label.setAttribute("for", "dias" + (index));
+            label.setAttribute("for", "dias" + index);
             label.textContent = dia;
 
             ulDia.appendChild(selectDia);
@@ -136,14 +143,20 @@ function seleccionDia() {
             ulDia.appendChild(document.createElement("br"));
         });
 
-        let seleccion2 = document.querySelectorAll('input[type="radio"][name="dias"]');
+        let seleccion2 = document.querySelectorAll(
+            'input[type="radio"][name="dias"]'
+        );
 
         seleccion2.forEach(function (radio) {
             radio.addEventListener("change", function () {
-                if (radio.checked) { diaSeleccionado = radio.value; }
+                if (radio.checked) {
+                    diaSeleccionado = radio.value;
+                }
                 agregarDia(diaSeleccionado);
             });
         });
+        let displayEspecialidad = document.getElementById("listaEspecialidad");
+        displayEspecialidad.style.display = "none";
     }
 }
 
@@ -155,26 +168,24 @@ function agregarDia(diaSeleccionado) {
     seleccionHora();
 }
 
-
-
-let horaSeleccionada
+let horaSeleccionada;
 
 function seleccionHora() {
     if (diaSeleccionado != undefined) {
         let ulHora = document.getElementById("listaHorarios");
 
         let spanHora = document.getElementById("spanHora");
-        spanHora.innerHTML = `Por favor seleccione un Horario disponible para su un turno en <br><br>`;
+        spanHora.innerHTML = `Por favor seleccione un Horario disponible para su un turno del dia <strong>${diaSeleccionado}</strong> en <strong>${especialidadSeleccionada}</strong> <br><br>`;
 
         horarios.forEach(function (hora, index) {
             let selectHora = document.createElement("input");
             selectHora.type = "radio";
             selectHora.name = "hora";
-            selectHora.id = "horas" + (index);
+            selectHora.id = "horas" + index;
             selectHora.value = `${hora}`;
 
             let label = document.createElement("label");
-            label.setAttribute("for", "horas" + (index));
+            label.setAttribute("for", "horas" + index);
             label.textContent = hora;
 
             ulHora.appendChild(selectHora);
@@ -182,14 +193,20 @@ function seleccionHora() {
             ulHora.appendChild(document.createElement("br"));
         });
 
-        let seleccion3 = document.querySelectorAll('input[type="radio"][name="hora"]');
+        let seleccion3 = document.querySelectorAll(
+            'input[type="radio"][name="hora"]'
+        );
 
         seleccion3.forEach(function (radio) {
             radio.addEventListener("change", function () {
-                if (radio.checked) { horaSeleccionada = radio.value; }
+                if (radio.checked) {
+                    horaSeleccionada = radio.value;
+                }
                 agregarHora(horaSeleccionada);
             });
         });
+        let displayDias = document.getElementById("listaDias");
+        displayDias.style.display = "none";
     }
 }
 
@@ -203,12 +220,16 @@ function agregarHora(horaSeleccionada) {
 
 function turnoFinal() {
     if (horaSeleccionada != undefined) {
+        let medicoFiltrado = medicos.filter(
+            (medicos) => medicos.servicio === especialidadSeleccionada
+        );
 
-        let medicoFiltrado = medicos.filter(medicos => medicos.servicio === especialidadSeleccionada);
-
-        let medico = medicoFiltrado.map(medicos => medicos.dr);
+        let medico = medicoFiltrado.map((medicos) => medicos.dr);
 
         let textoFinal = document.getElementById("turno");
-        textoFinal.innerHTML = `${nombrePaciente.toUpperCase()} ${apellidoPaciente.toUpperCase()} su turno quedo agendado con ${medico} de ${especialidadSeleccionada} el dia ${diaSeleccionado} a las ${horaSeleccionada}`;
+        textoFinal.innerHTML = `${nombrePaciente.toUpperCase()} ${apellidoPaciente.toUpperCase()}, su turno quedo agendado con: ${medico} de ${especialidadSeleccionada} el dia ${diaSeleccionado} a las ${horaSeleccionada}.`;
+
+        let displayHoras = document.getElementById("listaHorarios");
+        displayHoras.style.display = "none";
     }
-};
+}
